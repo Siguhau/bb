3069f147c84e5997d2ef5d9a2f9c746b53f38d35
Sigurd Hauan <sigurd.hauan@mazemap.com>
2026-08-23 20:24:03 +0200

    Add migration and simple migration script

---
+++ backend/prisma/migrations/20260823182004_add-new-service-types/migration.sql
@@ -0,0 +1,5 @@
+-- Add new service types to the ServiceType table
+INSERT INTO "ServiceType" ("code", "displayName") VALUES
+  ('TIRE_REPLACEMENT', 'Tire replacement'),
+  ('BOUVET_DELUXE_TUNE_UP', 'Bouvet Deluxe Tune-up'),
+  ('OTHER', 'Other');
